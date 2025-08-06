0d89f0af4aed48dfdb45de964304a1d9edd426f4
andrels17 <andreluis369369@hotmail.com>
2025-08-05 21:23:16 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,7 +3,14 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.io as pio
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+
+pio.kaleido.scope.chromium_args = [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+]
 
 # --------------- Configurações Gerais ---------------
 
